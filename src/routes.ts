afedf2656658cf0e3ffe4ada73020d3c97db0268
Jônattas Moraes <jonatas.moraes.09@hotmail.com>
2024-08-29 18:53:29 -0300

    feat(routes): add PATCH route for partial updates

--- src/routes.ts
+++ src/routes.ts
@@ -3,15 +3,23 @@ import { MeasureRepositoryPostgresImpl } from './repositories/MeasureRepositoryP
 import { pool } from './config/Postgres'
 import { CreateMeasureUseCase } from './useCases/createMeasureUseCase/CreateMeasureUseCase'
 import { CreateMeasureController } from './controllers/CreateMeasureController'
+import { PatchMeasureUseCase } from './useCases/patchMeasureUseCase/PatchMeasureUseCase'
+import { PatchMeasureController } from './controllers/PatchMeasureController'
 
 const router = Router()
 
 const measureRepository = new MeasureRepositoryPostgresImpl(pool)
 
 // Create measure and use case instance
-const measureUseCase = new CreateMeasureUseCase(measureRepository)
-const measureController = new CreateMeasureController(measureUseCase)
+const createUseCase = new CreateMeasureUseCase(measureRepository)
+const createController = new CreateMeasureController(createUseCase)
 
-router.post('/upload', (req, res) => measureController.createMeasure(req, res))
+// Patch measure and use case instance
+const patchUseCase = new PatchMeasureUseCase(measureRepository)
+const patchController = new PatchMeasureController(patchUseCase)
+
+// Routes
+router.post('/upload', (req, res) => createController.createMeasure(req, res))
+router.patch('/confirm', (req, res) => patchController.updateMeasure(req, res))
 
 export default router
