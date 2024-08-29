e35aaae2b42a38bae91ad79a6d5e2fbe96396676
Jônattas Moraes <jonatas.moraes.09@hotmail.com>
2024-08-29 16:58:42 -0300

    feat(provider): add Geminiprovider

---
+++ src/config/GeminiProvider.ts
@@ -0,0 +1,61 @@
+import { GoogleAIFileManager } from '@google/generative-ai/server'
+import { GoogleGenerativeAI } from '@google/generative-ai'
+import dotenv from 'dotenv'
+import fs from 'fs'
+import path from 'path'
+
+dotenv.config()
+
+export async function geminiProvider(imageBase64: string) {
+  const filePath = saveImageToFile(imageBase64)
+
+  try {
+    const fileUri = await uploadImage(filePath)
+    const generatedText = await generateTextFromImage(fileUri)
+
+    return { text: generatedText, uri: fileUri }
+  } catch (error: unknown) {
+    console.error(error)
+    throw error
+  } finally {
+    deleteFile(filePath)
+  }
+}
+
+function saveImageToFile(imageBase64: string): string {
+  const filePath = path.join(__dirname, 'temp.jpg')
+  fs.writeFileSync(filePath, Buffer.from(imageBase64, 'base64'))
+  return filePath
+}
+
+async function uploadImage(filePath: string): Promise<string> {
+  const fileManager = new GoogleAIFileManager(process.env.GEMINI_API_KEY!)
+  const {
+    file: { uri },
+  } = await fileManager.uploadFile(filePath, { mimeType: 'image/jpeg' })
+  return uri
+}
+
+async function generateTextFromImage(fileUri: string): Promise<string> {
+  const model = getGenerativeModel()
+  const result = await model.generateContent([
+    { fileData: { mimeType: 'image/jpeg', fileUri } },
+    {
+      text: 'Return only the sequence of numbers from the image, ignoring text and non-numeric characters.',
+    },
+  ])
+  return result.response.text()
+}
+
+function getGenerativeModel() {
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!)
+  return genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
+}
+
+function deleteFile(filePath: string): void {
+  try {
+    fs.unlinkSync(filePath)
+  } catch (error) {
+    console.error('Failed to delete temporary file:', error)
+  }
+}
