870869129fdb57666c50f88c200f081308a79557
Jônattas Moraes <jonatas.moraes.09@hotmail.com>
2024-08-29 17:29:32 -0300

    refactor(entity): remove date verification from Measure entity

--- src/domain/measure.ts
+++ src/domain/measure.ts
@@ -1,5 +1,4 @@
 import { v4 as uuid } from 'uuid'
-import moment from 'moment'
 import { SendError } from '../common/errors/SendError'
 
 export type MeasureType = 'WATER' | 'GAS'
@@ -37,15 +36,6 @@ function validate(props: Measure): void {
     )
   }
 
-  const formattedDate = dateValidator(props.measureDatetime?.toString())
-  if (!formattedDate) {
-    throw new SendError(
-      400,
-      'O measure_datetime não foi informado ou é invalido, por favor revise os dados e tente novamente',
-      'INVALID_DATA',
-    )
-  }
-
   if (!props.measureType || !['WATER', 'GAS'].includes(props.measureType)) {
     throw new SendError(
       400,
@@ -53,36 +43,4 @@ function validate(props: Measure): void {
       'INVALID_DATA',
     )
   }
-}
-
-function dateValidator(input: string): string | false {
-  // List of possible date formats
-  const formats = [
-    'YYYY-MM-DD', // year-month-day
-    'DD-MM-YYYY', // day-month-year
-    'DD/MM/YYYY', // day/month/year
-    'MM/DD/YYYY', // month/day/year
-    'YYYY-MM-DD HH:mm:ss', // year-month-day hour:minute:second
-    'DD/MM/YYYY HH:mm:ss', // day-month-year hour:minute:second
-    'MM/DD/YYYY HH:mm:ss', // month-day-year hour:minute:second
-    'YYYY-MM-DDTHH:mm:ssZ', // ISO format with Z for UTC
-    'YYYY-MM-DDTHH:mm:ss.SSSZ', // ISO format with milliseconds
-    'YYYY/MM/DD', // year/month/day
-    'YYYY.MM.DD', // year.month.day
-    'DD.MM.YYYY', // day.month.year
-    'DD-MMM-YYYY', // day-month name-year
-    'DD/MM/YYYY HH:mm', // day/month/year hour:minute
-    'MM/DD/YYYY HH:mm', // month/day/year hour:minute
-  ]
-
-  // Verify if the input matches any of the possible formats
-  for (const fmt of formats) {
-    const date = moment(input, fmt, true)
-    if (date.isValid()) {
-      return date.format('YYYY-MM-DDTHH:mm:ssZ')
-    }
-  }
-
-  // If none of the formats match, return false
-  return false
 }
