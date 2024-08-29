8e67de9adab7deec8928ca72b912f135ba3d2f5e
Jônattas Moraes <jonatas.moraes.09@hotmail.com>
2024-08-29 17:02:05 -0300

    feat(routes): add POST route for upload

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express'
+import { MeasureRepositoryPostgresImpl } from './repositories/MeasureRepositoryPostgresImpl'
+import { pool } from './config/Postgres'
+import { CreateMeasureUseCase } from './useCases/CreateMeasureUseCase'
+import { CreateMeasureController } from './controllers/CreateMeasureController'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const measureRepository = new MeasureRepositoryPostgresImpl(pool)
+
+// Create measure and use case instance
+const measureUseCase = new CreateMeasureUseCase(measureRepository)
+const measureController = new CreateMeasureController(measureUseCase)
+
+router.post('/upload', (req, res) => measureController.createMeasure(req, res))
 
 export default router
