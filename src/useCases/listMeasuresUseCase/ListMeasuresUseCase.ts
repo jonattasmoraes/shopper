332c57ba31c901289c3fff3252e2bf23d29f3fda
Jônattas Moraes <jonatas.moraes.09@hotmail.com>
2024-08-29 21:54:34 -0300

    feat:(use-case) integrate data relationship with DTO in list measures use case

---
+++ src/useCases/listMeasuresUseCase/ListMeasuresUseCase.ts
@@ -0,0 +1,46 @@
+import { SendError } from '../../common/errors/SendError'
+import { IMeasureRepository } from '../../domain/IMeasureRepository'
+import { Measure } from '../../domain/Measure'
+import { MeasuresDataDTO, MeasureDTO } from './ListMeasuresDTO'
+
+export class ListMeasuresUseCase {
+  constructor(private readonly listMeasureRepository: IMeasureRepository) {}
+
+  async findMeasures(code: string, type?: string): Promise<MeasuresDataDTO> {
+    try {
+      this.validateType(type)
+
+      const measures = await this.listMeasureRepository.listMeasures(code, type)
+
+      const measureDTOs = measures.map(this.toMeasureDTO)
+
+      return {
+        customer_code: code,
+        measures: measureDTOs,
+      }
+    } catch (error) {
+      console.error('Error retrieving measures:', error)
+      throw error
+    }
+  }
+
+  private validateType(type: string | undefined): void {
+    if (type && !this.isValidType(type)) {
+      throw new SendError(400, 'Tipo de medição não permitida', 'INVALID_TYPE')
+    }
+  }
+
+  private isValidType(type: string): boolean {
+    return type === 'WATER' || type === 'GAS'
+  }
+
+  private toMeasureDTO(measure: Measure): MeasureDTO {
+    return {
+      measure_uuid: measure.measureUuid,
+      measure_datetime: measure.measureDatetime,
+      measure_type: measure.measureType,
+      has_confirmed: measure.hasConfirmed,
+      image_url: measure.imageUrl,
+    }
+  }
+}
